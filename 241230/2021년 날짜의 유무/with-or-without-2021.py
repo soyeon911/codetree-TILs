a0ee288444150c3a6b232a068bd03070164fc8cc
박소연 <163836036+soyeon911@users.noreply.github.com>
2024-12-30 17:33:52 +0900

    Title: 2021년 날짜의 유무, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241230/2021년 날짜의 유무/with-or-without-2021.py
+++ 241230/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,22 +1,24 @@
 M, D = tuple(map(int, input().split()))
 
 def exist(M, D):
-    if M == 4 or M == 6 or M == 9 or M == 11:
-        if D >= 1 and D <= 30 :
-            return True
+    if M >= 1 and M <=12:
+        if M == 4 or M == 6 or M == 9 or M == 11:
+            if D >= 1 and D <= 30 :
+                return True
+            else:
+                return False
+        elif M == 2:
+            if D >= 1 and D <= 28:
+                return True
+            else:
+                return False
         else:
-            return False
-    elif M == 2:
-        if D >= 1 and D <= 28:
-            return True
-        else:
-            return False
+            if D >= 1 and D <= 31:
+                return True
+            else:
+                return False
     else:
-        if D >= 1 and D <= 31:
-            return True
-        else:
-            return False
-    return False
+        return False
 
 if exist(M, D):
     print("Yes")
