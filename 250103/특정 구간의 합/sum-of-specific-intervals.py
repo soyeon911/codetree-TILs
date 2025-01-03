e8e6b194e04af3f1e6a64d3e713eba3ad797eed1
박소연 <163836036+soyeon911@users.noreply.github.com>
2025-01-03 18:06:36 +0900

    Title: 특정 구간의 합, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250103/특정 구간의 합/sum-of-specific-intervals.py
+++ 250103/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,19 +1,16 @@
 A = []
 
 def range_sum(a1, a2):
-    return sum(A[a1 - 1:a2])  
-
+    return sum(A[a1 - 1: a2])
 
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
 results = []
 
-
 for _ in range(m):
     a1, a2 = map(int, input().split())
     results.append(range_sum(a1, a2))
-
 
 for result in results:
     print(result)
