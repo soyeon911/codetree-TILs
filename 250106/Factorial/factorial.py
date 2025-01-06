cba7b001858e0853ea1b404e59248e572aeea676
박소연 <163836036+soyeon911@users.noreply.github.com>
2025-01-06 16:39:01 +0900

    Title: Factorial, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250106/Factorial/factorial.py
+++ 250106/Factorial/factorial.py
@@ -4,10 +4,7 @@ def fac(n):
     if n == 0:
         return 1
 
-    if n == 1:
-        return 1
-    
-    return fac(n - 1) * (n % 10)
+    return fac(n - 1) * n
 
 result = fac(N)
 print(result)
