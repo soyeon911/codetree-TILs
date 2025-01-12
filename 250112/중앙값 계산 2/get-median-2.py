4168ab520ff2fb43549ad86bad1e4e7dd959ddf2
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-01-12 20:30:48 +0900

    Title: 중앙값 계산 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250112/중앙값 계산 2/get-median-2.py
+++ 250112/중앙값 계산 2/get-median-2.py
@@ -1,16 +1,22 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-def medium_val(n, arr):
-    result = []
-    temp = []
+# def medium_val(n, arr):
+#     result = []
+#     temp = []
 
-    for i in range(n):
-        temp.append(arr[i])
-        temp.sort()
-        if (i+1) % 2 != 0:
-            result.append(temp[len(temp) // 2])
-    return result        
+#     for i in range(n):
+#         temp.append(arr[i])
+#         temp.sort()
+#         if (i+1) % 2 != 0:
+#             result.append(temp[len(temp) // 2])
+#     return result        
 
-result = medium_val(n, arr)
-print(*result)
+# result = medium_val(n, arr)
+# print(*result)
+
+# 홀수 번째 수를 지날 때마다 정렬을 진행한 후 가운데 값을 출력함
+for i in range(n):
+    if i % 2 == 0:
+        sorted_arr = sorted(arr[:i + 1])
+        print(sorted_arr[i // 2], end = " ")
