271c83fa0fd85b77762012e0f03fdcd8d1accf2f
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-01-14 17:43:35 +0900

    Title: 비오는 날, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250114/비오는 날/rainy-day.py
+++ 250114/비오는 날/rainy-day.py
@@ -1,13 +1,4 @@
 n = int(input())
-date = []
-day = []
-weather = []
-
-for _ in range(n):
-    d, dy, w = input().split()
-    date.append(d)
-    day.append(dy)
-    weather.append(w)
 
 class WeatherCast:
     def __init__(self, date, day, weather):
@@ -15,16 +6,26 @@ class WeatherCast:
         self.day = day
         self.weather = weather
 
+ans = WeatherCast("9999-99-99", "", "")
+for _ in range(n):
+    date, day, weather = tuple(input().split())
+
+    f = WeatherCast(date, day, weather)
+    if weather == "Rain":
+        if ans.date >= f.date:
+            ans = f
+
+print(ans.date, ans.day, ans.weather)
 # 각 날짜별로 객체 생성
-weather_objects = [WeatherCast(date[i], day[i], weather[i]) for i in range(n)]
+#weather_objects = [WeatherCast(date[i], day[i], weather[i]) for i in range(n)]
 
 # 비가 오는 날만 필터링
-rainy_days = [obj for obj in weather_objects if obj.weather == "Rain"]
+#rainy_days = [obj for obj in weather_objects if obj.weather == "Rain"]
 
 # 결과 출력
-if rainy_days:
+#if rainy_days:
     # 날짜 기준으로 정렬 후 가장 빠른 날의 정보 출력
-    earliest_rainy_day = sorted(rainy_days, key=lambda x: x.date)[0]
-    print(f"{earliest_rainy_day.date} {earliest_rainy_day.day} {earliest_rainy_day.weather}")
-else:
-    print("No rainy days found.")
+ #   earliest_rainy_day = sorted(rainy_days, key=lambda x: x.date)[0]
+  #  print(f"{earliest_rainy_day.date} {earliest_rainy_day.day} {earliest_rainy_day.weather}")
+#else:
+ #   print("No rainy days found.")
