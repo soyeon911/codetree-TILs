b40d00b82a1276a136f440e178c553e49f562526
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-01-15 17:27:55 +0900

    Title: 총점 비교, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250115/총점 비교/compare-total-points.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+name = []
+score1 = []
+score2 = []
+score3 = []
+
+class Grade:
+    def __init__(self, name, score1, score2, score3):
+        self.name = name
+        self.score1, self.score2, self.score3 = int(score1), int(score2), int(score3)
+
+scores = []
+
+for _ in range(n):
+    name, score1, score2, score3 = input().split()
+    scores.append(Grade(name, score1, score2, score3))
+
+
+scores.sort(key=lambda x: (x.score1 + x.score2 + x.score3))
+
+for score in scores:
+    print(score.name, score.score1, score.score2, score.score3)
