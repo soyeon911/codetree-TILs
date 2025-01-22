74040edde634e9c18caaf2861e5753afc32b3a5d
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-01-22 16:22:09 +0900

    Title: 여러가지 진수변환, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250122/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,14 @@
+# N은 10진수, B는 진법
+n, q = map(int, input().split())
+
+def solution(n, q):
+    rev_base = ''
+
+    while n > 0:
+        n, mod = divmod(n, q)   # divmod() -> 두 값의 몫과 나머지를 tuple로 반환
+        rev_base += str(mod)
+
+    return rev_base[::-1] 
+    # 역순인 진수를 뒤집어 줘야 원래 변환 하고자하는 base가 출력
+    
+print(solution(n, q))
