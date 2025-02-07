9d6190c8f21486be792e66847b85dc184e270444
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-02-07 15:00:02 +0900

    Title: 사각형의 총 넓이 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250207/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 250207/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -10,6 +10,11 @@ for _ in range(n):
 
 square1 = (x2[0] - x1[0]) * (y2[0] - y1[0])
 square2 = (x2[1] - x1[1]) * (y2[1] - y1[1])
-overlap = (x2[0] - x1[1]) * (y2[1] - y1[1])
+
+# 겹치는 부분 넓이 계산
+overlap_width = max(0, min(x2[0], x2[1]) - max(x1[0], x1[1]))
+overlap_height = max(0, min(y2[0], y2[1]) - max(y1[0], y1[1]))
+overlap = overlap_width * overlap_height
+
 
 print(square1 + square2 - overlap)
