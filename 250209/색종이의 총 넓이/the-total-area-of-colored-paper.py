7332dbeedd74798e37eaef60eaba59fbbdbcf901
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-02-09 18:04:49 +0900

    Title: 색종이의 총 넓이, Time: 87ms, Memory: 48MB, Status: Passed - CodeTree

--- 250209/색종이의 총 넓이/the-total-area-of-colored-paper.py
+++ 250209/색종이의 총 넓이/the-total-area-of-colored-paper.py
@@ -1,19 +1,16 @@
 n = int(input())
 
-OFFSET = 1000  # 음수 좌표 처리를 위한 보정값
-SIZE = 2000  # 격자 크기 (좌표 범위 고려)
+OFFSET = 1000
+SIZE = 2000
 
-grid = [[0] * SIZE for _ in range(SIZE)]  # 2D 배열 (전체를 0으로 초기화)
+grid = [[0] * SIZE for _ in range(SIZE)]
 
 for _ in range(n):
     a, b = map(int, input().split())
 
-    # 좌표를 OFFSET을 적용하여 2D 배열에 매핑
     for i in range(a + OFFSET, a + 8 + OFFSET):
         for j in range(b + OFFSET, b + 8 + OFFSET):
-            grid[i][j] = 1  # 색칠
+            grid[i][j] = 1
 
-# 색칠된 부분(1의 개수) 세기
-total_area = sum(row.count(1) for row in grid)
-
-print(total_area)
+result = sum(row.count(1) for row in grid)
+print(result)
