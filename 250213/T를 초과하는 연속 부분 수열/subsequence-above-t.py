9bd64a0c17517c1891e945f4e833f37c4c499276
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-02-13 22:19:42 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250213/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 250213/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -1,19 +1,17 @@
-n, t = map(int, input().split())
-arr = list(map(int, input().split()))
+n, t = map(int, input().split())  # N: 수열 길이, T: 기준 숫자
+arr = list(map(int, input().split()))  # 수열 입력
 
-max_length = 1
-cnt = 1
+max_length = 0  # 최장 연속 부분 수열 길이
+cnt = 0  # 현재 연속 부분 수열의 길이
 
-for i in range(1, n):
-    if arr[i] > t:
-        if (arr[i] > arr[i - 1]):
-            cnt += 1
-        else:
-            max_length = max(max_length, cnt)
-            cnt = 1
+for i in range(n):
+    if arr[i] > t:  
+        cnt += 1  # T보다 크면 연속 부분 수열 길이 증가
     else:
-        cnt = 1
+        max_length = max(max_length, cnt)  # 최대 길이 갱신
+        cnt = 0  # 연속 부분 수열 초기화
 
+# 마지막 연속 부분 수열 고려
 max_length = max(max_length, cnt)
 
 print(max_length)
