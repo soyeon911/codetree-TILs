3d076072bfdf299826bc30f505785b61fbdd8282
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-02-14 14:00:45 +0900

    Title: 벌금은 누구에게, Time: 70ms, Memory: 16MB, Status: Passed - CodeTree

--- 250214/벌금은 누구에게/who-will-pay.py
+++ 250214/벌금은 누구에게/who-will-pay.py
@@ -2,9 +2,11 @@ N, M, K = map(int, input().split())
 student = [int(input()) for _ in range(M)]
 fines = [0] * (N + 1)
 
+ans = -1
 for s in student:
     fines[s] += 1
     
     if fines[s] == K:
-        print(s)
-        exit()
+        ans = s
+        break
+print(ans)
