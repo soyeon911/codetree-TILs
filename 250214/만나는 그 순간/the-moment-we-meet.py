86a14406885b3ad72c3fe8964c2a34b9655ab758
soyeon <163836036+soyeon911@users.noreply.github.com>
2025-02-14 13:36:06 +0900

    Title: 만나는 그 순간, Time: 122ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 250214/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,46 @@
+n, m = tuple(map(int, input().split()))
+
+a_arr = [tuple(input().split()) for _ in range(n)]
+b_arr = [tuple(input().split()) for _ in range(m)]
+
+a_ans_arr = []
+a_base = 0
+
+for direction, distance in a_arr:
+    distance = int(distance)
+    if direction == 'R':
+        while distance > 0:
+            a_base += 1
+            distance -= 1
+            a_ans_arr.append(a_base)
+    else:
+        while distance > 0:
+            a_base -= 1
+            distance -= 1
+            a_ans_arr.append(a_base)
+
+b_ans_arr = []
+b_base = 0
+
+for direction, distance in b_arr:
+    distance = int(distance)
+    if direction == 'R':
+        while distance > 0:
+            b_base += 1
+            distance -= 1
+            b_ans_arr.append(b_base)
+    else:
+        while distance > 0:
+            b_base -= 1
+            distance -= 1
+            b_ans_arr.append(b_base)
+
+result = 0
+
+for i in range(max(len(a_ans_arr), len(b_ans_arr))):
+    if a_ans_arr[i] == b_ans_arr[i]:
+        result = i + 1
+        print(result)
+        break
+    if i == max(len(a_ans_arr), len(b_ans_arr)) - 1:
+        print(-1)
